417b3e25d0acd6995d8d633a893782f906dd7eb7
ErickGoesSampaio <erickgoessampaio@gmail.com>
2022-09-14 10:02:19 -0300

    Desenvolvendo function e css

---
+++ wp-devs/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html <?php #language_atributes(); ?>>
+<head>
+    <meta charset="<?PHP bloginfo('charset');?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Suscipit tempora non adipisci quisquam cupiditate dolore
+          ipsum debitis. Blanditiis deleniti ratione voluptates?
+           Neque aliquid facilis voluptatem atque, sed dolorum saepe
+            fugit.</p>
+            <div id="page" class="site">
+                <header>
+                    <section class="top-bar">
+                        <div class="logo">
+                            Logo
+                        </div>
+                        <div class="searchbox">
+                                Searchbox
+                        </div>
+                    </section>
+                    <section class="menu-area">
+                        <nav class="main-menu">
+                            Menu
+                        </nav>
+                    </section>   
+                </header>
